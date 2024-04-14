8a97a3e28758b5f238c4d93663ac202501a5ecb5
Chinhdev <chinhdev@MacBook-Pro-cua-Chinhdev.local>
2024-04-14 17:12:05 +0700

    add: layout authentication

--- movie-project/src/app/(authentication)/layout.tsx
+++ movie-project/src/app/(authentication)/layout.tsx
@@ -1,7 +1,163 @@
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  IconProps,
+  Input,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+const avatars = [
+  {
+    name: "Ryan Florence",
+    url: "https://bit.ly/ryan-florence",
+  },
+  {
+    name: "Segun Adebayo",
+    url: "https://bit.ly/sage-adebayo",
+  },
+  {
+    name: "Kent Dodds",
+    url: "https://bit.ly/kent-c-dodds",
+  },
+  {
+    name: "Prosper Otemuyiwa",
+    url: "https://bit.ly/prosper-baba",
+  },
+  {
+    name: "Christian Nwamba",
+    url: "https://bit.ly/code-beast",
+  },
+];
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <>
+      <Box position={"relative"}>
+        <Container
+          as={SimpleGrid}
+          maxW="7xl"
+          columns={{ base: 1, md: 2 }}
+          spacing={{ base: 10, lg: 32 }}
+          py={{ base: 10, sm: 20, lg: 32 }}
+        >
+          <Stack spacing={{ base: 10, md: 20 }}>
+            <Heading
+              lineHeight={1.1}
+              fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "6xl" }}
+            >
+              Cùng nhau xem phim
+              <Text
+                as={"span"}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                bgClip="text"
+                mx={2}
+              >
+                &
+              </Text>
+              Kết nối mọi người cùng sở thích
+            </Heading>
+            <Stack direction={"row"} spacing={4} align={"center"}>
+              <AvatarGroup>
+                {avatars.map((avatar) => (
+                  <Avatar
+                    key={avatar.name}
+                    name={avatar.name}
+                    src={avatar.url}
+                    size="md"
+                    position={"relative"}
+                    zIndex={2}
+                    _before={{
+                      content: '""',
+                      width: "full",
+                      height: "full",
+                      rounded: "full",
+                      transform: "scale(1.125)",
+                      bgGradient: "linear(to-bl, red.400,pink.400)",
+                      position: "absolute",
+                      zIndex: -1,
+                      top: 0,
+                      left: 0,
+                    }}
+                  />
+                ))}
+              </AvatarGroup>
+              <Text
+                fontFamily={"heading"}
+                fontSize={{ base: "4xl", md: "6xl" }}
+              >
+                +
+              </Text>
+              <Flex
+                align={"center"}
+                justify={"center"}
+                fontFamily={"heading"}
+                fontSize={{ base: "sm", md: "lg" }}
+                bg={"gray.800"}
+                color={"white"}
+                rounded={"full"}
+                minWidth="60px"
+                minHeight="60px"
+                position={"relative"}
+                _before={{
+                  content: '""',
+                  width: "full",
+                  height: "full",
+                  rounded: "full",
+                  transform: "scale(1.125)",
+                  bgGradient: "linear(to-bl, orange.400,yellow.400)",
+                  position: "absolute",
+                  zIndex: -1,
+                  top: 0,
+                  left: 0,
+                }}
+              >
+                YOU
+              </Flex>
+            </Stack>
+          </Stack>
+          {children}
+        </Container>
+        <Blur
+          position={"absolute"}
+          top={-10}
+          left={-10}
+          style={{ filter: "blur(70px)" }}
+        />
+      </Box>
+    </>
+  );
 }
+
+const Blur = (props: IconProps) => {
+  return (
+    <Icon
+      width="40vw"
+      zIndex={-1}
+      height="560px"
+      viewBox="0 0 528 560"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle cx="71" cy="61" r="111" fill="#F56565" />
+      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
+      <circle cy="291" r="139" fill="#ED64A6" />
+      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
+      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
+      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
+      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+    </Icon>
+  );
+};
--- movie-project/src/app/(authentication)/user-register/page.tsx
+++ movie-project/src/app/(authentication)/user-register/page.tsx
@@ -1,3 +1,69 @@
+import { Box, Button, Heading, Input, Stack, Text } from "@chakra-ui/react";
+
 export default function UserRegisterPage() {
-  return <div>user register page</div>;
+  return (
+    <>
+      <Stack
+        bg={"gray.50"}
+        rounded={"xl"}
+        p={{ base: 4, sm: 6, md: 8 }}
+        spacing={{ base: 8 }}
+        maxW={{ lg: "lg" }}
+      >
+        <Stack spacing={4}>
+          <Heading color="gray.800" fontSize="3xl">
+            ĐĂNG KÝ NGAY
+          </Heading>
+          <Text color="gray.500" fontSize="md">
+            Bạn có thể lưu trữ phim, chia sẻ, nhắn tin với tài khoản của mình
+          </Text>
+        </Stack>
+        <Box as="form">
+          <Stack spacing={4}>
+            <Input
+              placeholder="Firstname"
+              bg={"gray.100"}
+              border={0}
+              color={"gray.500"}
+              _placeholder={{
+                color: "gray.500",
+              }}
+            />
+            <Input
+              placeholder="firstname@lastname.io"
+              bg={"gray.100"}
+              border={0}
+              color={"gray.500"}
+              _placeholder={{
+                color: "gray.500",
+              }}
+            />
+            <Input
+              placeholder="+1 (___) __-___-___"
+              bg={"gray.100"}
+              border={0}
+              color={"gray.500"}
+              _placeholder={{
+                color: "gray.500",
+              }}
+            />
+          </Stack>
+          <Button
+            fontFamily={"heading"}
+            mt={8}
+            w={"full"}
+            bgGradient="linear(to-r, red.400,pink.400)"
+            color={"white"}
+            _hover={{
+              bgGradient: "linear(to-r, red.400,pink.400)",
+              boxShadow: "xl",
+            }}
+          >
+            ĐĂNG KÝ NGAY
+          </Button>
+        </Box>
+        form
+      </Stack>
+    </>
+  );
 }
