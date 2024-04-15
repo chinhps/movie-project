9bf4a4b3e0a9197d1e29856e441da2feaa498dd9
Chinhdev <chinhdev@MacBook-Pro-cua-Chinhdev.local>
2024-04-15 19:27:11 +0700

    add: controller Category, Movie, Comment

--- api-movie-project/routes/api.php
+++ api-movie-project/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Movie\CommentController;
+use App\Http\Controllers\Movie\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix("movies")->group(function () {
+    Route::get("/", [MovieController::class, 'movies']);
+    Route::get("latest", [MovieController::class, 'moviesLatest']);
+    Route::get("ranking", [MovieController::class, 'moviesRanking']);
+
+    Route::get("detail/{slug}", [MovieController::class, 'movieDetail']);
+});
+
+Route::prefix("comments")->group(function () {
+    Route::get("movie/{slug}", [CommentController::class, 'commentMovie']);
+});
+
+Route::prefix("categories")->group(function () {
+    Route::get("/", [CategoryController::class, 'list']);
+    Route::get("{slug}", [CategoryController::class, 'detail']);
 });
