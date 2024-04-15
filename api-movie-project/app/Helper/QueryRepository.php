9cad5787d81249604eb64e0b1a6a5108862d6afa
Chinhdev <chinhdev@MacBook-Pro-cua-Chinhdev.local>
2024-04-15 19:25:14 +0700

    add: helper

---
+++ api-movie-project/app/Helper/QueryRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Eloquent\Builder;
+
+if (!function_exists('queryRepository')) {
+    function queryRepository(Builder $model, array $filter)
+    {
+        if (isset($filter['query'])) {
+            $model->where($filter['query']);
+        }
+        if (isset($filter['sort'])) {
+            foreach ($filter['sort'] as $valueSort) {
+                $model->orderBy($valueSort[0], $valueSort[1]);
+            }
+        }
+        if (isset($filter['between'])) {
+            $model->whereBetween(
+                $filter['between']['column'],
+                [
+                    $filter['between']['between'][0],
+                    $filter['between']['between'][1]
+                ]
+            );
+        }
+        if (!isset($filter['sort'])) {
+            $model->orderBy('id', 'desc');
+        }
+
+        return $model;
+    };
+}
