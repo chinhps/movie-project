8a4d213beb7c105c1259269f7ac63df1fba4604d
chinhdev <phamchinh347@gmail.com>
2024-05-30 00:00:29 +0700

    fix: response json

--- server-m3u8/upload-file.php
+++ server-m3u8/upload-file.php
@@ -9,7 +9,7 @@ header('Content-Type: application/json');
 // Check if the file is an m3u8 file
 if ($fileType != "m3u8") {
     $response_data = array(
-        'status' => '413',
+        'status' => 413,
         'message' => "Only m3u8 files are allowed.",
     );
     echo json_encode($response_data);
@@ -19,7 +19,7 @@ if ($fileType != "m3u8") {
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     $response_data = array(
-        'status' => '413',
+        'status' => 413,
         'message' => "Sorry, your file is too large.",
     );
     echo json_encode($response_data);
@@ -29,7 +29,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     $response_data = array(
-        'status' => '413',
+        'status' => 413,
         'message' => "Your file was not uploaded.",
     );
     echo json_encode($response_data);
@@ -37,14 +37,14 @@ if ($uploadOk == 0) {
     // Verify the file and move it to the final directory
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         $response_data = array(
-            'status' => 'success',
+            'status' => 200,
             'message' => 'File uploaded successfully',
             'fileName' => $target_file
         );
         echo json_encode($response_data);
     } else {
         $response_data = array(
-            'status' => '413',
+            'status' => 413,
             'message' => "There was an error uploading your file.",
         );
         echo json_encode($response_data);
