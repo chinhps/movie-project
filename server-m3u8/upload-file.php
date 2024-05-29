864d84e7983db399b0050675a7e0c0e4e41b65d2
chinhdev <phamchinh347@gmail.com>
2024-05-29 22:15:54 +0700

    add: server-m3u8

---
+++ server-m3u8/upload-file.php
@@ -0,0 +1,52 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . md5(uniqid(rand(), true)) . '.m3u8';
+$uploadOk = 1;
+$fileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
+
+header('Content-Type: application/json');
+
+// Check if the file is an m3u8 file
+if ($fileType != "m3u8") {
+    $response_data = array(
+        'status' => '413',
+        'message' => "Only m3u8 files are allowed.",
+    );
+    echo json_encode($response_data);
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    $response_data = array(
+        'status' => '413',
+        'message' => "Sorry, your file is too large.",
+    );
+    echo json_encode($response_data);
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    $response_data = array(
+        'status' => '413',
+        'message' => "Your file was not uploaded.",
+    );
+    echo json_encode($response_data);
+} else {
+    // Verify the file and move it to the final directory
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        $response_data = array(
+            'status' => 'success',
+            'message' => 'File uploaded successfully',
+            'fileName' => $target_file
+        );
+        echo json_encode($response_data);
+    } else {
+        $response_data = array(
+            'status' => '413',
+            'message' => "There was an error uploading your file.",
+        );
+        echo json_encode($response_data);
+    }
+}
