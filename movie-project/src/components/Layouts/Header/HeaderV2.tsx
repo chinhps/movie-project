75d9371586c90ed0f923a43683aa7e62800603d1
chinhdev <phamchinh347@gmail.com>
2024-09-19 19:52:04 +0700

    test: demo ci/cd to production

--- movie-project/src/components/Layouts/Header/HeaderV2.tsx
+++ movie-project/src/components/Layouts/Header/HeaderV2.tsx
@@ -25,7 +25,7 @@ function HeaderV2() {
       >
         <Heading as="h1" fontSize={{ base: "1.5rem", md: "2rem" }}>
           <BlurText text={"ANIMEFU NƠI VỪA ĐỂ GIẢI TRÍ"} delay={20} />
-          <BlurText text={"VÀ LÀ NỀN TẢNG HỌC TẬP TIẾNG ANH 123"} delay={40} />
+          <BlurText text={"VÀ LÀ NỀN TẢNG HỌC TẬP TIẾNG ANH"} delay={40} />
         </Heading>
         <GradientText
           colors={["#111111", "#2C8CC2"]} // Custom gradient colors
@@ -35,7 +35,7 @@ function HeaderV2() {
           maxWidth="550px"
           w="100%"
         >
-          Không chỉ là một nền tảng học tập mà còn là nơi mang lại niềm vui và
+          Không chỉ là 234523423423 một nền tảng học tập mà còn là nơi mang lại niềm vui và
           động lực cho người dùng thông qua các bộ anime hấp dẫn.
         </GradientText>
         <Magnet padding={50} disabled={false}>
